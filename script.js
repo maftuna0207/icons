1a020bd02faf78a9f4fc57357367e1e55428057f
maftuna0207 <86225161+maftuna0207@users.noreply.github.com>
2022-02-13 17:12:33 +0500

    icons

---
+++ script.js
@@ -0,0 +1,14 @@
+const icon1 = document.querySelector(".icon-2");
+const icon2 = document.querySelector(".icon-1");
+icon1.addEventListener("mouseover", (e) => {
+  icon1.classList.toggle("active");
+  icon2.style.display = "block";
+  icon1.style.display = "none";
+ 
+});
+icon2.addEventListener("mouseout",()=>{
+    icon1.style.display= "block"
+    icon2.style.display="none"
+
+
+});
